c613f031e6c967f1bb375b0c70d4e3ae56137d92
JiwebD <gkdlfnzo12@naver.com>
2025-02-25 17:50:08 +0900

    Day002 2025-02-25 Ch02 자료형, 상수

--- MY_JAVABOOK/src/Ch02/C04자료형.java
+++ MY_JAVABOOK/src/Ch02/C04자료형.java
@@ -57,18 +57,94 @@ public class C04자료형 {
 		//double : 8byte 실수(15-18자리),기본자료형
 		
 		//정밀도 확인		종류 : 단정밀도, 배정밀도
-		float n1 = 0.123456789123456789F; //f,F:float형 접미사
-		double n2 = 0.123456789123456789;
+//		float n1 = 0.123456789123456789F; //f,F:float형 접미사
+//		double n2 = 0.123456789123456789;
+//		
+//		System.out.println(n1);
+//		System.out.println(n2);
+//		//오차 확인
+//		float num = 0.1F;
+//		for(int i=0;i<=1E5;i++) {
+//			num=num+0.1F;
+//			System.out.println(i);
+//		}
+//		System.out.println("num: " + num);
 		
-		System.out.println(n1);
-		System.out.println(n2);
-		//오차 확인
-		float num = 0.1F;
-		for(int i=0;i<=1E10;i++) {
-			num=num+0.1F;
-			System.out.println(i);
-		}
-		System.out.println("num: " + num);
+		//--------------------
+		//단일문자 char 2byte 정수
+		//--------------------
+//		char ch1 = 'a';
+//		System.out.println(ch1);
+//		System.out.println((int)ch1); // 011000001
+//	
+//		
+//		char ch2 = 98;
+//		System.out.println(ch2);
+//		System.out.println((int)ch2); // 011000010
+//		
+//		char ch3 = 'b'+1;
+//		System.out.println(ch3);
+//		System.out.println((int)ch3); // 011000011
+//		
+//		byte ch4 = 'c'+2;
+//		System.out.println((char)ch4);
+//		System.out.println(ch4); // 011000011
+//		
+//		char ch5  = 0xac93;
+//		char ch6 = 0b1010110010010011;
+//		
+//		System.out.println(ch5);
+//		System.out.println(ch6);
+//		System.out.println((char)(0b1010110010010011+1));
+		 
+//		char ch6 = 0xac00+1;
+//		System.out.printf("%c %c\n", ch5,ch6);
+//		
+//		// \\u :유니코드값 이스케이프문자
+//		System.out.printf("%c\n", '\uAC93');
+//		
+//		System.out.printf("TEST : %c\n", '\uabcd');
+//		
+//		
+//		char n = 10;
+//		System.out.printf("HELLO %c WORLD", n);
+		
+		
+		//--------------------
+		//문자열 : String (클래스자료형)
+		//--------------------
+		
+//		기본자료형(원시타입)
+//		byte n1;
+//		short n2;
+//		double n3;
+//		long n4;
+		
+		//클래스자료형
+		//클래스자료형으로 만든변수는 '참조변수'라고 하고
+		//참조변수는 데이터가 저장된 위치정보(메모리주소값)이 저장된다.
+//		int n1 = 10;
+//		byte n2 = 20;
+//		char n3 = 40;
+//		
+		String name = "홍길동";
+		String job = "프로그래머";
+		System.out.println(name);
+		System.out.println(job);
+
+		//--------------------
+		//boolean : 논리형(true/false 저장)
+		//--------------------
+		
+//		boolean flag = (10>11); 	// 참(긍정)
+//		if(flag) 
+//		{
+//			System.out.println("참인경우 실행");
+//		}
+//		else 
+//		{
+//			System.out.println("거짓인경우 실행");
+//		}
 		
 	}
 
---
+++ MY_JAVABOOK/src/Ch02/C06정리_문제풀기.java
@@ -0,0 +1,71 @@
+package Ch02;
+
+public class C06정리_문제풀기 {
+
+	public static void main(String[] args) {
+//		정수 타입 		: byte,short,int ,long
+//		char 타입 	: ' ' 
+//		String 타입 	: " "
+//		실수 타입 		: double , float
+//		논리 타입 		: boolean
+
+		
+//		확인 문제1
+
+//		정수타입 
+//		1byte : byte
+		byte n1 = 127;
+//		byte n1 = 128;  //에러남 1000 0000  =>맨 앞 비트가 음수부호라 
+		System.out.println(n1);
+//		2byte : char / short		
+		char n2 = 65535; //(0~2^16-1)	(0~65535)
+		short n3 = 32767;
+		short n4 = -32768;
+		System.out.println(n2);
+		System.out.println(n3);
+		System.out.println(n4);
+//		4byte : int 
+		int n5 = 1;
+		System.out.println(n5);
+//		8byte : long 
+		long n6 = 10000000000L;
+		System.out.println(n6);
+//		실수타입
+//		4byte : float
+		float n7 = 0.123456789123456789F;
+		System.out.println(n7);
+//		8byte : double
+		double n8 = 0.123456789123456789;
+		System.out.println(n8);
+
+//		논리타입
+//		1byte : boolean
+		boolean flag = (10>11);
+		if(flag) {
+			System.out.println("참 : true");
+		}
+		else {
+			System.out.println("거짓 : false");
+		}
+
+		
+//		확인문제2
+
+		
+//		맞는 코드인지 틀린 코드인지 확인 
+//		byte var = 200;							( x	)
+//		char var='AB';							( x	)
+//		char var=65;							( x	)
+//		long var=50000000000;					( x	)	
+//		float var = 3.14						( x )
+//		double var = 100.0						( x	)
+//		String var = "나의직업은 "개발자" 입니다.";		( x )
+//		boolean var = 0;						( x	)
+//		int v2 = 1e2;							( x )
+//		float =1e2f;							( x	)
+		
+//		System.out.println(var);
+
+	}
+
+}
