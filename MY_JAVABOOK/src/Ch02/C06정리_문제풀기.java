04c85d2d50e9c96a94c1196a06e3a6db77fb65aa
JiwebD <gkdlfnzo12@naver.com>
2025-02-26 09:18:30 +0900

    Day003 2025-02-26

--- MY_JAVABOOK/src/Ch02/C06정리_문제풀기.java
+++ MY_JAVABOOK/src/Ch02/C06정리_문제풀기.java
@@ -18,7 +18,7 @@ public class C06정리_문제풀기 {
 //		byte n1 = 128;  //에러남 1000 0000  =>맨 앞 비트가 음수부호라 
 		System.out.println(n1);
 //		2byte : char / short		
-		char n2 = 65535; //(0~2^16-1)	(0~65535)
+		char n2 = 51111; //(0~2^16-1)	(0~65535)
 		short n3 = 32767;
 		short n4 = -32768;
 		System.out.println(n2);
@@ -53,16 +53,16 @@ public class C06정리_문제풀기 {
 
 		
 //		맞는 코드인지 틀린 코드인지 확인 
-//		byte var = 200;							( x	)
-//		char var='AB';							( x	)
-//		char var=65;							( o )
-//		long var=50000000000;					( x	)	
-//		float var = 3.14						( x )
-//		double var = 100.0						( o	)
-//		String var = "나의직업은 "개발자" 입니다.";		( x )
-//		boolean var = 0;						( x	)
-//		int v2 = 1e2;							( x )
-//		float =1e2f;							( x	)
+//		byte var = 200;							//( x )
+//		char var='AB';							//( x )
+//		char var=65;							//( o )
+//		long var=50000000000;					//( x )	
+//		float var = 3.14;						//( x )
+//		double var = 100.0						//( o )
+//		String var = "나의직업은 "개발자" 입니다.";		//( x )
+//		boolean var = 0;						//( x )
+//		int v2 = 1e2;							//( x )
+//		float =1e2f;							//( x )
 		
 //		System.out.println(var);
 
