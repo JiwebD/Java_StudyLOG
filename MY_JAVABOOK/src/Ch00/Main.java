3ce47088049412172777941baf02377993f5ded9
JiwebD <gkdlfnzo12@naver.com>
2025-03-13 14:03:15 +0900

    Day 013 2025-03-13 Java2 .

--- MY_JAVABOOK/src/Ch00/Main.java
+++ MY_JAVABOOK/src/Ch00/Main.java
@@ -1,49 +1,29 @@
 package Ch00;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-import java.util.*;
-
-public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int C = sc.nextInt();
-		
-
-		if(60-B<C%60) {
-			A++;
-			B = B+C%60;
-			if(A>23) {
-				A=0;
-			}
-			if(B>59) {
-				B = (B+C%60)-60;
-			}
-			System.out.println(A + " " + B);
-		}
-		else if(C/60 > 0) {
-			A += C/60;
-			
-			if(60-B<C%60) {
-				A++;
-			}
-			B = B+C%60;
-			
-			if(A>23) {
-				A=0;
-			}
-			if(B>59) {
-				B = (B+C%60)-60;
-			}
-			System.out.println(A + " " + B);
-		}
-		else {
-			System.out.println(A + " " + (B+C));
-			
-		}
-
-
-	}
+public class Main{
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st;
+        
+        for(int i=0; i<T;i++){
+            st = new StringTokenizer(br.readLine()," ");
+            bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+"\n");
+        }
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }
